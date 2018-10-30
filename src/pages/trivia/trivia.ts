4010eec8351899bedb45a225da2c542d4bbf1675
wichitav <lindavcuestas@gmail.com>
2018-10-30 17:42:42 -0500

    log in, sing up y primeras paginas funcionando

--- src/pages/trivia/trivia.ts
+++ src/pages/trivia/trivia.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-trivia',
+  templateUrl: 'trivia.html'
 })
-export class HomePage {
+export class TriviaPage {
 
   constructor(public navCtrl: NavController) {
   }
